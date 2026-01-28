fd97a9dd4998f7d1627cca799fc282184ea17cab
ibougajd <ilyassbougajdi1337@gmail.com>
2026-01-28 19:03:59 +0100

    +elk stack +grafana & prometheus

--- services/auth/src/app.module.ts
+++ services/auth/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-    imports: [],
+    imports: [
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            host: 'postgres_db',
+            port: 5432,
+            username: process.env.DB_USER,
+            password: process.env.DB_PASS,
+            database: 'auth_db',
+            entities: [],
+            synchronize: true,
+        }),
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
