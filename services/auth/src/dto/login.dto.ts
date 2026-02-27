7da6a3854aa86719233449b8b757197b92d680dd
younesonj <younesounajjary@gmail.com>
2026-02-27 03:46:36 +0000

    updating login method

--- services/auth/src/dto/login.dto.ts
+++ services/auth/src/dto/login.dto.ts
@@ -1,9 +1,9 @@
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class LoginDto {
-  @IsEmail()
+  @IsString()
   @IsNotEmpty()
-  email: string;
+  identifier: string;  // ← Changed from "email" to "identifier"
 
   @IsString()
   @IsNotEmpty()
